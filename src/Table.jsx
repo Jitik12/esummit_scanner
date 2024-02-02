faa2a44e87bb82800e003506e661d3300a4ecca3
Jitik12 <shaikarmaan2004@gmail.com>
2024-02-02 12:35:31 +0530

    Armaan : added the footfall

--- src/Table.jsx
+++ src/Table.jsx
@@ -1,13 +1,13 @@
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import { useState } from 'react';
-import axios from 'axios';
-import { Box } from '@mui/material';
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { useState } from "react";
+import axios from "axios";
+import { Box, Typography } from "@mui/material";
 
 export default function BasicTable() {
   const columns = ["Name", "Email", "Phone", "Status"];
@@ -22,53 +22,70 @@ export default function BasicTable() {
   // ];
 
   const [rows, setRows] = useState([]);
+  const [count, setCount] = useState(0);
 
-  async function fetch(){
+  async function fetch() {
     try {
-      const res = await axios.get('https://scanner.iith-ac.in:8000/get_all_data/');
+      const res = await axios.get(
+        "https://scanner.iith-ac.in:8000/get_all_data/"
+      );
       setRows(res.data);
-    }
-    catch (err) {
+      setCount(setCount(rows.length));
+    } catch (err) {
       console.log(err);
     }
     setTimeout(fetch, 1000, []);
   }
 
-  if (!done){
+  if (!done) {
     fetch();
     setDone(true);
   }
 
   return (
     <Box margin="2rem">
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            {
-              columns.map((col, i) => (<TableCell key={i}><Box>{col}</Box></TableCell>))
-            }
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row, i) => (
-            <TableRow
-              key={i}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-              style={{ backgroundColor: row[3] ? '#80ff80' : '#ff8686' }} // Conditionally set background color
-            >
-              <TableCell><Box>{row[0]}</Box></TableCell>
-              <TableCell><Box>{row[1]}</Box></TableCell>
-              <TableCell><Box>{row[2]}</Box></TableCell>
-              <TableCell><Box>{row[3] ? "in" : "out"}</Box></TableCell>
+      <Box>
+        <Typography variant="h6" color={"green"}>
+          Number of people registered : {count}
+        </Typography>
+      </Box>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              {columns.map((col, i) => (
+                <TableCell key={i}>
+                  <Box>{col}</Box>
+                </TableCell>
+              ))}
             </TableRow>
-          ))}
+          </TableHead>
+          <TableBody>
+            {rows.map((row, i) => (
+              <TableRow
+                key={i}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                style={{ backgroundColor: row[3] ? "#80ff80" : "#ff8686" }} // Conditionally set background color
+              >
+                <TableCell>
+                  <Box>{row[0]}</Box>
+                </TableCell>
+                <TableCell>
+                  <Box>{row[1]}</Box>
+                </TableCell>
+                <TableCell>
+                  <Box>{row[2]}</Box>
+                </TableCell>
+                <TableCell>
+                  <Box>{row[3] ? "in" : "out"}</Box>
+                </TableCell>
+              </TableRow>
+            ))}
 
-          {/* ))} */}
-        </TableBody>
-      </Table>
-    </TableContainer>
+            {/* ))} */}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </Box>
   );
 }
-  
